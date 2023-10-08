eabd84f6435d35aa819f9b33ed5da9accb9b42c0
johne <johnywalker696@gmail.com>
2023-10-08 16:11:06 -0300

    PDOStatement object(query) com fetchall

--- index.php
+++ index.php
@@ -9,48 +9,18 @@
     try{
         $conexao = new PDO($dsn, $usuario, $senha);
 
-        /*$query = '
-            create table if not exists tb_usuarios(
-                id int not null primary key auto_increment,
-                nome varchar(50) not null,
-                email varchar(100) not null,
-                senha varchar(32) not null
-        )';
-
-        $retorno = $conexao->exec($query);
-        //0
-        echo $retorno; */
-
         $query = '
-           insert into tb_usuarios(
-                nome, email, senha
-           )values(
-                "jamiltom damasceno", "jamiltom@gmail.com", "123456"
-           )
+            select * from tb_usuarios
         ';
 
-        $conexao->exec($query);
+        $stmt = $conexao->query($query);
+        $lista = $stmt->fetchAll();
 
-        $query = '
-           insert into tb_usuarios(
-                nome, email, senha
-           )values(
-                "lucas", "lucas@gmail.com", "987654"
-           )
-        ';
+        echo '<pre>';
+        print_r($lista);
+        echo '</pre>';
 
-        $conexao->exec($query);
-
-        $query = '
-           insert into tb_usuarios(
-                nome, email, senha
-           )values(
-                "kety", "kety@gmail.com", "123123"
-           )
-        ';
-
-        $conexao->exec($query);
-        
+        echo $lista[0]['email'];
 
     } catch(PDOException $e){                           //$e de erro!
         echo 'Erro ' . $e->getCode() .'Mensagem: ' . $e->getMessage(); 
